9fc4cb4241e1aa9040d856207ec256c9b98dbffb
KB-Gen-Ai <mmkb_1972@yahoo.com>
2025-04-14 22:06:31 +0300

    Create qr_generator.py

---
+++ qr_generator.py
@@ -0,0 +1,17 @@
+import qrcode
+from io import BytesIO
+
+def generate_qr_code(url):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color="black", back_color="white")
+    buf = BytesIO()
+    img.save(buf)
+    return buf.getvalue()
